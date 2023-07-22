8f4eb2168fc9a3c2458981047db12c02b494eee0
Akhil Athuluri <8309889800a@gmail.com>
2023-07-23 02:22:03 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -62,7 +62,7 @@ const Portfolio = () => {
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
+        <img
             src={image}
             style={{
               height: "90%",
